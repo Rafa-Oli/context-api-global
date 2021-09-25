deac0d6b49fef6f36a5d8d81f63e11f10a679b69
Rafaela Oliveira <rafaela.albaniza@gmail.com>
2021-09-24 21:10:36 -0300

    feat: create hook custom

--- src/common/contexts/CartProvider.tsx
+++ src/common/contexts/CartProvider.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { IProduct } from 'components/Product/product';
+import { useContext, useState } from 'react';
 import { CartContext } from './Cart';
 
 export const CartProvider = ({ children }: any) => {
@@ -8,4 +9,23 @@ export const CartProvider = ({ children }: any) => {
       {children}
     </CartContext.Provider>
   );
+};
+
+export const UserCartContext = () => {
+  const { cart, setCart } = useContext(CartContext);
+
+  function addProduct(newProduct: IProduct) {
+    const hasProduct = cart.some((itemCart) => itemCart.id === newProduct.id);
+    if (!hasProduct) {
+      newProduct.quantidade = 1;
+      return setCart([...cart, newProduct]);
+    }
+    setCart(
+      cart.map((item: IProduct) => {
+        if (item.id === newProduct.id) (item.quantidade as number) += 1;
+        return item;
+      })
+    );
+  }
+  return { cart, setCart, addProduct };
 };
--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -2,34 +2,13 @@ import { Container } from './styles';
 import { IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-import { useContext } from 'react';
-import { CartContext } from 'common/contexts/Cart';
+import { IProduct } from './product';
+import { UserCartContext } from 'common/contexts/CartProvider';
 
-interface Product {
-  name: string;
-  photo: string;
-  quantidade?: number;
-  value: number;
-  id: number;
-  unidade?: number;
-}
+function Product({ name, photo, id, value, unidade }: IProduct) {
+  const { cart, addProduct } = UserCartContext();
+  const productInCart = cart.find((item) => item.id === id);
 
-function Product({ name, photo, id, value, unidade }: Product) {
-  const { cart, setCart } = useContext(CartContext);
-
-  function addProduct(newProduct: Product) {
-    const hasProduct = cart.some((itemCart) => itemCart.id === newProduct.id);
-    if (!hasProduct) {
-      newProduct.quantidade = 1;
-      return setCart([...cart, newProduct]);
-    }
-    setCart(
-      cart.map((item: any) => {
-        if (item.id === newProduct.id) item.quantidade += 1;
-        return item;
-      })
-    );
-  }
   return (
     <Container>
       <div>
@@ -42,6 +21,7 @@ function Product({ name, photo, id, value, unidade }: Product) {
         <IconButton color='secondary'>
           <RemoveIcon />
         </IconButton>
+        {productInCart?.quantidade || 0}
         <IconButton onClick={() => addProduct({ name, photo, id, value })}>
           <AddIcon />
         </IconButton>
